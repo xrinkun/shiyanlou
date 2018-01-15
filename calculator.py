e456376229d52663745a97febfdc84b8d426a3ca
739402544 <739402544@qq.com>
2018-01-15 21:39:21 +0800

    Jan-15

--- calculator.py
+++ calculator.py
@@ -2,40 +2,54 @@
 import sys
 class config(object):
     def __init__(self):
-        self._config = {}
+        self.config = {}
         args = sys.argv[1:]
         index = args.index('-c')
         self.configfile = args[index+1]
-    def get_config():
+    def _get_config(self):
         with open(self.configfile) as configf:
-            configf.read()
-            self._config = configf.strip().split('=')
-    def Shu(self):
-        self.config = self._config
+            for line in configf.readlines():
+                key,value = configf.strip().split('=')
+            try:
+                    config[key.strip()] = float(value.strip())
+            except ValueError:
+                    print("Parameter Error")
+                    exit()                    
+            return config
+    @property
+    def config_give(self):
+        return self._get_config
 class salary(object):
     def __init__(self):
-        self._sa = {}
+        self.salary_dict = {}
         args = sys.argv[1:]
         index = args.index['-d']
-        self.salaflie = argv[index+1]
+        self.salafile = argv[index+1]
     def getsala(self):
         with open(self.salafile) as sfile:
-            sfile.read()
-            self._sa = sfile.split(',')
-    def sala(self):
-        self.sala = self._sa
+            for line in sfile.readlines():
+                key,value = sfile.split(',')
+            try:
+                self.salary_dict[key.strip()] = float(value.strip())
+            except ValueError:
+                print("ParameterError")
+                exit()
+            return salary_dict
+    @property
+    def salary_give(self):
+        return salary.getsala
 def insert(s):
     args = sys.argv[1:]
     index = args['-o']
-    sflie = args[index+1]
-    li = salary.getsala()
-    flie = open('~/Gongzi.csv','w')
-    flie.write(li.append(s))
-    flie.close()
+    sfile = args[index+1]
+    file = open('sflie','w')
+    for line in file.readlines('sflie'):
+        file.write(list(flie.readlines('sflie')).append(s))
+    file.close()
 class cal(object):
     def bao(self):
-        config = config.Shu
-        sala = salary.sala
+        config = config.config_give
+        sala = salary.salary_give
         for x in sala:
             if sala[x] < config['JiShuL']:
                 xian = config['JiShuL'] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])
@@ -43,7 +57,7 @@ class cal(object):
                 xian = config['JiShuH'] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])  
             else:
                 xian = sala[x] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])  
-            insert(xian)
+            return insert(xian)
     def result(self,n,xian):
         if n <= 3500:
             r = format(n-n*xian, ".2f")    
@@ -61,10 +75,20 @@ class cal(object):
             r = format(n-(n-xian-3500) * 0.35 + 5505 + n*xian, ".2f")
         else:
             r = format(n-(n-xian-3500) * 0.45 + 13505 + n*xian, ".2f")
-        insert(r)
+        return insert(r)
     def ge(self,qian,bao,result):
         g = qian - bao - result
-        insert(g)
-cal.bao
-cal.result
-cal.ge
+        return insert(g)
+if __name__ == "__main__":
+    try:
+        args = sys.argv[1:]
+        index = args['-o']
+        sfile = args[index+1]
+        with open(salary.salary_give) as copy:
+            for line in copy.readlines():
+                file('sfile','w')
+    except:
+        print('Error')
+    cal.bao
+    cal.ge
+    cal.result
