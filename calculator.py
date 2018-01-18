0ceff4affdf9ee44271e02b72344256be08b8060
739402544 <739402544@qq.com>
2018-01-18 11:33:16 +0800

    New

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import csv
 class config(object):
     def __init__(self):
         self.config = {}
@@ -38,18 +39,17 @@ class salary(object):
     @property
     def salary_give(self):
         return salary.getsala
-def insert(s):
-    args = sys.argv[1:]
-    index = args['-o']
-    sfile = args[index+1]
-    file = open('sflie','w')
-    for line in file.readlines('sflie'):
-        file.write(list(flie.readlines('sflie')).append(s))
-    file.close()
+def insert(s,inx):            
+    inx -= 1
+    with open(salary.salafile) as li:
+        list_re = li.readlines().split(',').strip()
+    list_re[inx] = list_re[inx] + s
+    return list_re
 class cal(object):
     def bao(self):
         config = config.config_give
         sala = salary.salary_give
+        inx++
         for x in sala:
             if sala[x] < config['JiShuL']:
                 xian = config['JiShuL'] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])
@@ -57,38 +57,44 @@ class cal(object):
                 xian = config['JiShuH'] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])  
             else:
                 xian = sala[x] * (config['YangLao']+config['YiLiao']+config['Shiye']+config['GongShang']+config['ShengYu']+config['GongJiJin'])  
-            return insert(xian)
-    def result(self,n,xian):
-        if n <= 3500:
-            r = format(n-n*xian, ".2f")    
-        elif n <= 5000:
-            r = format(n-(n-xian-3500) * 0.03 + n*xian, ".2f")
-        elif n <= 8000:
-            r = format(n-(n-xian-3500) * 0.1 + 105 + n*xian, ".2f")
-        elif n <= 12500:
-            r = format(n-(n-xian-3500) * 0.2 + 555 + n*xian, ".2f")
-        elif n <= 38500:
-            r = format(n-(n-xian-3500) * 0.25 + 1005 + n*xian, ".2f")
-        elif n <= 58500:
-            r = format(n-(n-xian-3500) * 0.3 + 2775 + n*xian, ".2f")
-        elif n <= 83500:
-            r = format(n-(n-xian-3500) * 0.35 + 5505 + n*xian, ".2f")
-        else:
-            r = format(n-(n-xian-3500) * 0.45 + 13505 + n*xian, ".2f")
-        return insert(r)
-    def ge(self,qian,bao,result):
+            return insert(xian),inx
+    @property 
+    def bao_xian():
+        return bao().xian
+    def result(self):
+        sala = salary.salary_give
+        xian = bao_xian
+        inx++
+        for x in sala:
+            n = sala[x]
+            if n <= 3500:
+                r = format(n-n*xian, ".2f")    
+            elif n <= 5000:
+                r = format(n-(n-xian-3500) * 0.03 + n*xian, ".2f")
+            elif n <= 8000:
+                r = format(n-(n-xian-3500) * 0.1 + 105 + n*xian, ".2f")
+            elif n <= 12500:
+                r = format(n-(n-xian-3500) * 0.2 + 555 + n*xian, ".2f")
+            elif n <= 38500:
+                r = format(n-(n-xian-3500) * 0.25 + 1005 + n*xian, ".2f")
+            elif n <= 58500:
+                r = format(n-(n-xian-3500) * 0.3 + 2775 + n*xian, ".2f")
+            elif n <= 83500:
+                r = format(n-(n-xian-3500) * 0.35 + 5505 + n*xian, ".2f")
+            else:
+                r = format(n-(n-xian-3500) * 0.45 + 13505 + n*xian, ".2f")
+            return insert(r),inx
+    @property
+    def result_r():
+        return result.r
+    def ge(self):
+        inx ++
+        sala = salary.salary_give
+        xian = bao_xian
+        result = result_r
         g = qian - bao - result
         return insert(g)
 if __name__ == "__main__":
-    try:
-        args = sys.argv[1:]
-        index = args['-o']
-        sfile = args[index+1]
-        with open(salary.salary_give) as copy:
-            for line in copy.readlines():
-                file('sfile','w')
-    except:
-        print('Error')
-    cal.bao
-    cal.ge
-    cal.result
+    config.config_give
+    salary.salary_give
+    cal.bao()
